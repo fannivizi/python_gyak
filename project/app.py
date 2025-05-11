c9ee76f5562e9dd6e84f33788e3eafde5937e5bf
Vizi Fanni <h379474@stud.u-szeged.hu>
2025-05-11 13:16:43 +0200

    reg + login works

--- project/app.py
+++ project/app.py
@@ -1,12 +1,21 @@
+import os
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User, Watched
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '\x92\x8e(\x145\x8b\xcdti\xed\xd4y'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data', 'users.db')}"
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 def best_rated(tv_shows):
-    return sorted([s for s in tv_shows if s['weight']],
-                    key=lambda s: s['weight'],
-                    reverse=True)
+    tv_shows.sort(key=lambda show: show['weight'], reverse=True)
+    return tv_shows
 
 @app.route('/')
 def landing():
@@ -40,6 +49,45 @@ def show(show_id):
 
     return render_template("show.html", show=tv_show, seasons=seasons)
 
+@app.route('/reg')
+def reg():
+    error = ""
+
+    if request.method == 'POST':
+        username=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+
+        if username.strip() == "" or email.strip() == "" or password.strip() == "":
+            error = "Adj meg minden adatot!"
+        elif User.query.filter_by(username=username).first():
+            error = "A felhasználónév foglalt."
+        elif User.query.filter_by(email=email).first():
+            error = "Az email cím már foglalt."
+
+        if error == "":
+            pw = generate_password_hash(password)
+            db.session.add(User(username=username, email=email, password=pw))
+            db.session.commit()
+            flash("Sikeres regisztráció")
+            return redirect("/")
+
+    return render_template('reg.html', error=error)
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password, password):
+        session['username'] = user.username
+        flash("Sikeres bejelentkezés")
+        return redirect("/")
+
+    flash("Hibás felhasználónév vagy jelszó")
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run()
---
+++ project/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    username = db.Column(db.String(80), primary_key=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class Watched(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
