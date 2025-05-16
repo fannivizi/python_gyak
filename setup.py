8e6beda1427f3b6e3be1c4038c9e73ff60d90518
Vizi Fanni <h379474@stud.u-szeged.hu>
2025-05-16 23:33:07 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.config.expand import find_packages
 
 setup(
     name='python_gyak',
     version='',
     packages=find_packages(exclude=("tests",)),
+    package_dir={'': 'python_gyak'},
     url='',
     license='',
     author='f4nni',
